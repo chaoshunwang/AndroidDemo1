db98b664b263f1dd6d68e4ac3a3fffcdc109ff9e
wangchaoshun <wangchaoshun@hd123.com>
2017-07-10 11:44:05 +0800

    test

--- app/src/main/java/com/wcs/demo/MainActivity.java
+++ app/src/main/java/com/wcs/demo/MainActivity.java
@@ -1,10 +1,17 @@
 package com.wcs.demo;
 
-import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by wangchaoshun on 2016/3/1.
  */
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
 }
