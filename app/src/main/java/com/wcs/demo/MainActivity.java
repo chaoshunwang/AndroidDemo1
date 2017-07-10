890a57da0157e3f20abbb032dc71e0871d8651a3
wangchaoshun <wangchaoshun@hd123.com>
2017-07-10 11:54:15 +0800

    test

--- app/src/main/java/com/wcs/demo/MainActivity.java
+++ app/src/main/java/com/wcs/demo/MainActivity.java
@@ -3,6 +3,7 @@ package com.wcs.demo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 /**
  * Created by wangchaoshun on 2016/3/1.
@@ -13,5 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        Log.i("wcstest", "ssssssssssssss");
     }
 }
